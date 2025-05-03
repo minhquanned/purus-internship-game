895c766ebf27407d0b39b3ae010bd4bdb915d1fc
Minh Quan <minhquan4so9@gmail.com>
2025-05-03 13:38:44 +0700

    maybe last time

--- src/components/GameManager.js
+++ src/components/GameManager.js
@@ -135,7 +135,7 @@ export class GameManager {
     }
 
     initializePlayer() {
-        this.player = new Player(this.app, `${BASE_PATH}/models/Mage.glb`, `${BASE_PATH}/textures/mage_texture.png`, 0.4, []);
+        this.player = new Player(this.app, `${BASE_PATH}/assets/models/Mage.glb`, `${BASE_PATH}/assets/textures/mage_texture.png`, 0.4, []);
     }
 
     initializeEnemyManager() {
